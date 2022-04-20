5e7881bf2e21950a5e9537a1c999ec7d5d38c0fb
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-20 17:55:40 +0300

    Modify sign and login routes and modify template styling

--- hood/urls.py
+++ hood/urls.py
@@ -4,6 +4,6 @@ from .views import index, signupUser, loginUser
 
 urlpatterns = [
     path('', index, name='index'),
-    path('signup', signupUser, name='signup'),
-    path('login', loginUser, name='login')
+    path('signup/', signupUser, name='signup'),
+    path('login/', loginUser, name='login')
 ]

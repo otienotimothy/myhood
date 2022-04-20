a9a53ee7a6675e852a3cac6a7bac35b3af2818d1
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-20 17:37:49 +0300

    Add view functions for creating a user and login a user

--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
+
+from .forms import UserRegistrationForm, LoginUserForm
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def signupUser(request):
+    form = UserRegistrationForm()
+    context = {'form': form}
+    return render(request, 'signup.html', context)
+
+def loginUser(request):
+    form = LoginUserForm()
+    context = {'form': form}
+    return render(request, 'login.html', context)

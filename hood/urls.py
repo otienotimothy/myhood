e1f6b7a592e4953a5f541c70df88055e63d513e5
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-21 11:45:04 +0300

    Add logout route

--- hood/urls.py
+++ hood/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import index, home, signupUser, loginUser, createJoinHood, createHood, joinHood
+from .views import index, home, signupUser, loginUser, logoutUser, createJoinHood, createHood, joinHood
 
 urlpatterns = [
     path('', index, name='index'),
     path('hood/<str:hood>/', home, name='home' ),
     path('signup/', signupUser, name='signup'),
     path('login/', loginUser, name='login'),
+    path('logout/', logoutUser, name='logout'),
     path('create-join-hood/', createJoinHood, name='create-join-hood'),
     path('create-hood/', createHood, name='create-hood' ),
     path('join-hood/', joinHood, name='join-hood' )

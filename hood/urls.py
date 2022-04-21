d3e89c1199ce9cb259a4042a570f695503698b43
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-21 11:37:53 +0300

    Add create hood and join hood routes

--- hood/urls.py
+++ hood/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import index, signupUser, loginUser, createJoinHood
+from .views import index, home, signupUser, loginUser, createJoinHood, createHood, joinHood
 
 urlpatterns = [
     path('', index, name='index'),
+    path('hood/<str:hood>/', home, name='home' ),
     path('signup/', signupUser, name='signup'),
     path('login/', loginUser, name='login'),
-    path('create-join-hood/', createJoinHood, name='create-join-hood')
+    path('create-join-hood/', createJoinHood, name='create-join-hood'),
+    path('create-hood/', createHood, name='create-hood' ),
+    path('join-hood/', joinHood, name='join-hood' )
 ]

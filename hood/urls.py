c0a76cdf98cba2ada3708fd47ee2ce1c73940fd8
Timothy Otieno <otienotimothyomondi@gmail.com>
2022-04-21 07:02:37 +0300

    Add route for the create/join template

--- hood/urls.py
+++ hood/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index, signupUser, loginUser
+from .views import index, signupUser, loginUser, createJoinHood
 
 urlpatterns = [
     path('', index, name='index'),
     path('signup/', signupUser, name='signup'),
-    path('login/', loginUser, name='login')
+    path('login/', loginUser, name='login'),
+    path('create-join-hood/', createJoinHood, name='create-join-hood')
 ]
